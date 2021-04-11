11b2bddd336b9420fdc699e44d10495094303833
Frantz Paul <fpaul787@gmail.com>
2021-04-11 19:36:25 -0400

    created input bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Frantz App';
   description = 'Hello World';
+  hero = 'Drogfisher';
 }
--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import {Input} from '@angular/core';
+
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.css']
 })
+
+
 export class HeroComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  @Input () name: string;
 
 }
